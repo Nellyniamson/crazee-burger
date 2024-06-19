02352c8684835977835d85628c0a40015febb50e
Nelly niamson <niamsonnelly@gmail.com>
2024-06-19 21:27:01 +0000

    F03 changes

--- src/components/pages/reusable-ui/Logo.jsx
+++ src/components/pages/reusable-ui/Logo.jsx
@@ -11,5 +11,27 @@ export default function Logo() {
 }
 
 const LogoStyled = styled.div`
-  background-color : blue;
-`;
+    /* background-color : blue; */
+    display: flex;
+    align-items: center;
+
+    h1{
+        display: inline;
+        text-align: center;
+        color: #ffa01b;
+        font-size: 36px;
+        line-height: 1em;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 1.5px;
+        font-family: "Amatic SC" , cursive;
+    }
+
+    img{
+        object-fit: contain;
+        object-position: center;
+        height: 60px;
+        width: 80px;
+        margin: 0 5px ;
+    }
+`
